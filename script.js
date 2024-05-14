020f67754af0caa42b33f150c984c055352ed504
Caleforty <82247028+Caleforty@users.noreply.github.com>
2024-05-14 16:18:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-document.getElementById('submit').addEventListener('click', function() {
-    const product = document.getElementById('product').value.split('-');
-    const price = parseFloat(product[0]);
-    const bagsPerHour = parseInt(product[1]);
-    const hourlySalary = parseFloat(document.getElementById('hourlySalary').value);
+document.getElementById('calculate').addEventListener('click', function () {
+    const productSelect = document.getElementById('product');
+    const salaryInput = document.getElementById('salary');
+
+    const [productPrice, productBagsPerHour] = productSelect.value.split(',').map(Number);
+    const hourlySalary = Number(salaryInput.value);
+
     const workers = 3;
+    const hourlyCost = hourlySalary * workers;
+    const defaultBagsPerHour = 15;
+    const roiBags = Math.ceil(productPrice / ((productBagsPerHour - defaultBagsPerHour) * hourlyCost));
 
-    const totalHourlyCost = hourlySalary * workers;
-    const bagsToMix = Math.ceil(price / (totalHourlyCost / bagsPerHour));
-
-    document.getElementById('bagsToMix').innerText = bagsToMix;
-    document.getElementById('form').classList.add('hidden');
+    document.getElementById('bags-result').innerText = roiBags;
+    document.querySelector('.input-group').style.display = 'none';
+    document.getElementById('calculate').style.display = 'none';
     document.getElementById('result').classList.remove('hidden');
 });
 
-document.getElementById('startOver').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.getElementById('form').classList.remove('hidden');
+document.getElementById('start-over').addEventListener('click', function (e) {
+    e.preventDefault();
+    document.getElementById('product').value = '3285,45';
+    document.getElementById('salary').value = '40';
+    document.querySelector('.input-group').style.display = 'block';
+    document.getElementById('calculate').style.display = 'block';
     document.getElementById('result').classList.add('hidden');
 });
